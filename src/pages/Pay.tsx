c7227e96674a05b8a602df7d72f55b2b80fb4fae
ckoopmann <c.k.e.koopmann@gmail.com>
2023-04-22 10:25:22 +0800

    Use back camera on mobile for payment scan

--- src/pages/Pay.tsx
+++ src/pages/Pay.tsx
@@ -1,49 +1,55 @@
-import {useState} from 'react'
-import {TextareaAutosize} from '@mui/material'
-import { QrReader } from 'react-qr-reader';
+import { useState } from "react";
+import { TextareaAutosize } from "@mui/material";
+import { QrReader } from "react-qr-reader";
 import { useNavigate } from "react-router-dom";
 
 function Pay() {
     const navigate = useNavigate();
 
-    const [qrscan, setQrscan] = useState('No result');
+    const [qrscan, setQrscan] = useState("No result");
 
     const handleResult = (result: any, error: any) => {
         if (result) {
-            setQrscan(result)
+            setQrscan(result);
             const targetUrl = `/payConfirmation?${result}`;
             console.log("targetUrl:", targetUrl);
-            navigate(targetUrl)
+            navigate(targetUrl);
         }
         if (error) {
-            console.error(error)
+            console.error(error);
         }
-    }
+    };
 
     return (
-      <div>
+        <div>
             <span>QR Scanner</span>
-            
+
             <center>
-            <div style={{marginTop:30}}>
-                <QrReader
-                    scanDelay={300}
-                    onResult={handleResult}
-                    constraints={{ height: 320, width: 320 }}
-                />
-            </div>
+                <div style={{ marginTop: 30 }}>
+                    <QrReader
+                        scanDelay={300}
+                        onResult={handleResult}
+                        constraints={{
+                            height: 320,
+                            width: 320,
+                            facingMode: "environment",
+                        }}
+                    />
+                </div>
             </center>
 
             <TextareaAutosize
-                style={{fontSize:18, width:320, height:100, marginTop:100}}
+                style={{
+                    fontSize: 18,
+                    width: 320,
+                    height: 100,
+                    marginTop: 100,
+                }}
                 defaultValue={qrscan}
                 value={qrscan}
             />
-
-      </div>
+        </div>
     );
-  }
-  
-  export default Pay;
-  
+}
 
+export default Pay;
